2a40dbfb12846323ac973e0585a947fd0a55c979
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-04-29 22:58:01 +0300

    Update trainings.go

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,13 +1,78 @@
 package trainings
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type Training struct {
-	// TODO: добавить поля
+	Steps        int
+	TrainingType string
+	Duration     time.Duration
+	personaldata.Personal
 }
 
-func (t *Training) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+func (t *Training) Parse(datastring string) error {
+	data := strings.Split(datastring, ",")
+	if len(data) != 3 {
+		return fmt.Errorf("invalid format")
+	}
+
+	steps, err := strconv.Atoi(data[0])
+	if err != nil || steps <= 0 {
+		return fmt.Errorf("invalid steps")
+	}
+
+	trainingType := strings.TrimSpace(data[1])
+	if trainingType == "" {
+		return fmt.Errorf("invalid training type")
+	}
+
+	durStr := strings.TrimSpace(data[2])
+	dur, err := time.ParseDuration(durStr)
+	if err != nil || dur <= 0 {
+		return fmt.Errorf("invalid duration")
+	}
+
+	t.Steps = steps
+	t.TrainingType = trainingType
+	t.Duration = dur
+	return nil
 }
 
 func (t Training) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	dist := spentenergy.Distance(t.Steps, t.Height)
+	speed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
+
+	var calories float64
+	var err error
+
+	switch t.TrainingType {
+	case "Бег":
+		calories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	case "Ходьба":
+		calories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	default:
+		return "", fmt.Errorf("неизвестный тип тренировки")
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	result := fmt.Sprintf(
+		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
+		t.TrainingType,
+		t.Duration.Hours(),
+		dist,
+		speed,
+		calories,
+	)
+
+	return result, nil
 }
