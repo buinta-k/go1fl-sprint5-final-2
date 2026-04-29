6101f54e6d22f34b38f4ac5df7f10478583cf8c7
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-04-29 22:59:17 +0300

    Update actioninfo.go

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,31 @@
 package actioninfo
 
+
+import (
+	"fmt"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(datastring string) error
+    ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	    for _, data := range dataset {
+        
+        err := dp.Parse(data)
+        if err != nil {
+            fmt.Printf("ошибка обработки данных '%s': %v\n", data, err)
+            continue
+        }
+
+        
+        info, err := dp.ActionInfo()
+        if err != nil {
+            fmt.Printf("ошибка обработки данных '%s': %v\n", data, err)
+            continue
+        }
+
+        fmt.Println(info)
+    }
 }
