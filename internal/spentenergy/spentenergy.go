44f1ef9dedbed24036610f60797a7d3eca992410
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-05-01 13:06:35 +0300

    Update spentenergy.go

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -14,16 +14,36 @@ const (
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps<=0 || weight<=0 || height<=0 || duration<=0 {
+		return 0, fmt.Errorf("Неккоректный параметр")
+	}
+	speed:=MeanSpeed(steps,height,duration)
+	calory:=(weight*speed)/(duration.Minutes())
+	return calory*walkingCaloriesCoefficient, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	if steps<=0 || weight<=0 || height<=0 || duration<=0 {
+		return 0, fmt.Errorf("Неккоректный параметр")
+	}
+	speed:=MeanSpeed(steps,height,duration)
+	durationInMinutes:=duration.Minutes()
+	return =(weight*speed*durationInMinutes)/minInH, nil
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	if steps<0 || duration<=0 {
+		return 0
+	}
+	distance:=Distance(steps,height)
+	return distance/duration.Hours()
 }
 
 func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+	if steps<=0 || height<=0 {
+		return 0
+	}
+	length:=height*stepLengthCoefficient
+	result:=length*float64(steps)
+	return result/mInKm
 }
