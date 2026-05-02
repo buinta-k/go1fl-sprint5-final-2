70c5b2e8e4fffc5d3341476c565b9d2621c513c0
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-05-02 21:49:02 +0300

    Update personaldata.go

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -12,7 +12,7 @@ type Personal struct {
 
 func (p Personal) Print() {
 	fmt.Printf(
-		"Имя: %s\nВес: %.2f кг.\nРост: %.2fм.\n",
+		"Имя: %s\nВес: %.2f\nРост: %.2f\n",
 		p.Name,
 		p.Weight,
 		p.Height,
