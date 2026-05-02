3bae61248e935a0bd7f37b97dfe567f6175a34c5
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-05-02 13:35:54 +0300

    Update trainings.go

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,13 +1,53 @@
 package trainings
 
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
 type Training struct {
-	// TODO: добавить поля
+	Steps int
+	TrainingType string
+	Duration time.duration
+	personaldata.Personal
 }
 
 func (t *Training) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	str:=strings.Split(datastring,",")
+	if len(str)!=3 {
+		return fmt.Errorf("Длина слайса не равна 3")
+	}
+	t.Steps,err:=strconv.Atoi(str[0]) 
+	if err!=nil{
+		return fmt.Errorf("Ошибка преобразования string в int")
+	}
+	t.TrainingType=str[1]
+	hours, err2:=time.ParseDuration(str[2])
+	if err2!=nil {
+		fmt.Errorf("Ошибка преобразования string в duration")
+	}
 }
 
 func (t Training) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance:=Distance(t.Steps, t.Height)
+	speed:=MeanSpeed(t.Steps, t.Height, t.Duration)
+
+	if t.TrainingType=="Бег" {
+		calory:=RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	}
+	if t.TrainingType=="Ходьба" {
+		calory:=WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	} else {
+		return "",fmt.Errorf("Неизвестный тип тренировки")
+	}
+	result:=fmt.Sprintf(
+		"Тип тренировки: %s\nДлительность: %.f2 ч.\nДистанция: %d км.\nСкорость: %.f2 км/ч\nСожгли калорий: %.f2\n",
+		t.TrainingType,
+		t.Duration.Hours(),
+		distance,
+		speed,
+		calory
+	)
+	return result, nil
 }
