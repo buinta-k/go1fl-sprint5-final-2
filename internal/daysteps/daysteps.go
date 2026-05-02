401c723837d8119baaf278d0404836094de625bc
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-05-02 14:02:17 +0300

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,48 @@
 package daysteps
 
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps int
+	Duration time.duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	data:=strings.Split(datastring,",")
+	if len(data)!=2 {
+		fmt.Errorf("Длина слайса не равна 2")
+	}
+	steps,err2:=strconv.Atoi(data[0])
+	if err2!=nil {
+		fmt.Errorf("Ошибка преобразования string в int")
+	}
+	hours,err3:=time.ParseDuration(data[1])
+	if err3!=nil {
+		fmt.Errorf("Ошибка преобразования string в duration")
+	}
+
+	ds.Steps=steps
+	ds.Duration=hours
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance:=Distance(ds.Steps,ds.Height)
+	calory, err:=WalkingSpentCalories(ds.Steps,ds.Weight,ds.Height,ds.Duration)
+	
+	if err!=nil {
+		return 0, fmt.Errorf("Ошибка вычисления функции")
+	}
+	
+	result:=fmt.Sprintf(
+		"Количество шагов %d.\nДистанция составила %.f2 км.\nВы сожгли %.2f ккал.\n",
+		ds.Steps,
+		distance,
+		calory
+	)
+	return result, nil
 }
