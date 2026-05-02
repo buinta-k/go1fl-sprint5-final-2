6a89454109068d4525f8a1a446786841a376643b
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-05-02 21:59:22 +0300

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -12,34 +12,35 @@ import (
 
 type DaySteps struct {
 	Steps int
-	Duration time.duration
+	Duration time.Duration
 	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	data:=strings.Split(datastring,",")
 	if len(data)!=2 {
-		fmt.Errorf("Длина слайса не равна 2")
+		return fmt.Errorf("Длина слайса не равна 2")
 	}
 	steps,err2:=strconv.Atoi(data[0])
 	if err2!=nil {
-		fmt.Errorf("Ошибка преобразования string в int")
+		return fmt.Errorf("Ошибка преобразования string в int")
 	}
 	hours,err3:=time.ParseDuration(data[1])
 	if err3!=nil {
-		fmt.Errorf("Ошибка преобразования string в duration")
+		return fmt.Errorf("Ошибка преобразования string в duration")
 	}
 
 	ds.Steps=steps
 	ds.Duration=hours
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	distance:=Distance(ds.Steps,ds.Height)
-	calory, err:=WalkingSpentCalories(ds.Steps,ds.Weight,ds.Height,ds.Duration)
+	distance:=spentenergy.Distance(ds.Steps,ds.Height)
+	calory, err:=spentenergy.WalkingSpentCalories(ds.Steps,ds.Weight,ds.Height,ds.Duration)
 	
 	if err!=nil {
-		return 0, fmt.Errorf("Ошибка вычисления функции")
+		return "", fmt.Errorf("Ошибка вычисления функции")
 	}
 	
 	result:=fmt.Sprintf(
