173279f2e05e5c117482c9746a1d82fcfb09fab0
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-05-03 21:01:45 +0300

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -6,13 +6,12 @@ import (
 	"strings"
 	"time"
 
-	
-	"github.com/Yandex-Practicum/tracker/internal/personaldata"
-	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+	"://github.com"
+	"://github.com"
 )
 
 type DaySteps struct {
-	Steps int
+	Steps    int
 	Duration time.Duration
 	personaldata.Personal
 }
@@ -20,23 +19,25 @@ type DaySteps struct {
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	data := strings.Split(datastring, ",")
 	if len(data) != 2 {
-		return fmt.Errorf("Длина слайса не равна 2")
+		return fmt.Errorf("длина слайса не равна 2")
 	}
 
 	steps, err2 := strconv.Atoi(data[0])
 	if err2 != nil {
-		return fmt.Errorf("Ошибка преобразования string в int")
+		return fmt.Errorf("ошибка преобразования string в int")
 	}
+
 	if steps <= 0 {
-		return fmt.Errorf("количество шагов должно быть больше нуля")
+		return fmt.Errorf("шаги должны быть больше 0")
 	}
 
 	hours, err3 := time.ParseDuration(data[1])
 	if err3 != nil {
-		return fmt.Errorf("Ошибка преобразования string в duration")
+		return fmt.Errorf("ошибка преобразования string в duration")
 	}
+
 	if hours <= 0 {
-		return fmt.Errorf("длительность должна быть больше нуля")
+		return fmt.Errorf("длительность должна быть больше 0")
 	}
 
 	ds.Steps = steps
@@ -45,18 +46,15 @@ func (ds *DaySteps) Parse(datastring string) (err error) {
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	distance:=spentenergy.Distance(ds.Steps,ds.Height)
-	calory, err:=spentenergy.WalkingSpentCalories(ds.Steps,ds.Weight,ds.Height,ds.Duration)
-	
-	if err!=nil {
-		return "", fmt.Errorf("Ошибка вычисления функции")
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	calory, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return "", fmt.Errorf("ошибка вычисления функции")
 	}
-	
-	result:=fmt.Sprintf(
-		"Количество шагов %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
-		ds.Steps,
-		distance,
-		calory
+
+	result := fmt.Sprintf(
+		"Количество шагов: %d\nДистанция составила: %.2f км\nВы сожгли: %.2f ккал\n",
+		ds.Steps, distance, calory,
 	)
 	return result, nil
 }
