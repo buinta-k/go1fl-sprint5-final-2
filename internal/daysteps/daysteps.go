3bc39d26399912008d6f328534f9f8e61c54fed8
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-05-03 21:24:25 +0300

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -53,7 +53,7 @@ func (ds DaySteps) ActionInfo() (string, error) {
 	}
 
 result := fmt.Sprintf(
-	"Количество шагов: %d.\nДистанция: %.2f км.\nСожгли калорий: %.2f.\n",
+	"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
 	ds.Steps, distance, calory,
 )
 	return result, nil
