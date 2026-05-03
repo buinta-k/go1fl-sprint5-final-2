9b80d233e63e73587973d4045c41ed3c1c879234
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-05-03 21:18:26 +0300

    Update spentenergy.go

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -7,45 +7,55 @@ import (
 
 // Основные константы, необходимые для расчетов.
 const (
-	mInKm                      = 1000 // количество метров в километре.
-	minInH                     = 60   // количество минут в часе.
-	stepLengthCoefficient      = 0.45 // коэффициент для расчета длины шага на основе роста.
-	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
+	mInKm                      = 1000
+	minInH                     = 60
+	stepLengthCoefficient      = 0.45
+	walkingCaloriesCoefficient = 0.5
 )
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
-	if steps<=0 || weight<=0 || height<=0 || duration<=0 {
-		return 0, fmt.Errorf("Неккоректный параметр")
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("некорректные параметры")
 	}
-	speed:=MeanSpeed(steps,height,duration)
+
+	speed := MeanSpeed(steps, height, duration)
 	durationInMinutes := duration.Minutes()
-	result := (weight * speed * durationInMinutes) / minInH
-	return result * walkingCaloriesCoefficient, nil
+
+	calories := (weight * speed * durationInMinutes) / minInH
+	calories *= walkingCaloriesCoefficient
+
+	return calories, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-    if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
-        return 0, fmt.Errorf("некорректные параметры")
-    }
-    speed := MeanSpeed(steps, height, duration)
-    calories := (runningCaloriesCoefficient1*speed - runningCaloriesCoefficient2) * weight / minInH * duration.Minutes()
-    return calories, nil
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("некорректные параметры")
+	}
+
+	speed := MeanSpeed(steps, height, duration)
+	durationInMinutes := duration.Minutes()
+
+	calories := (weight * speed * durationInMinutes) / minInH
+
+	return calories, nil
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	if steps<0 || duration<=0 {
+	if steps < 0 || duration <= 0 {
 		return 0
 	}
-	distance:=Distance(steps,height)
-	return distance/duration.Hours()
+
+	distance := Distance(steps, height)
+	return distance / duration.Hours()
 }
 
 func Distance(steps int, height float64) float64 {
-	if steps<=0 || height<=0 {
+	if steps <= 0 || height <= 0 {
 		return 0
 	}
-	length:=height*stepLengthCoefficient
-	result:=length*float64(steps)
-	return result/mInKm
+
+	stepLength := height * stepLengthCoefficient
+	distance := stepLength * float64(steps)
+
+	return distance / mInKm
 }
